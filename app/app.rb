08272a4e3695191acb772acd77492e87b1b99d53
cgulli <caitlin.gulliford@gmail.com>
2016-10-31 14:38:41 +0000

    test commit

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,8 @@ class HeartbreakBnB < Sinatra::Base
     'Hello HeartbreakBnB!'
   end
 
+  heyyyyyyyy girl heyyyyy
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
