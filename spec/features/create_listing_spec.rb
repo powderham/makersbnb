e02365e6c75bfb50c4e1c92aebac18fce6816de1
Ed Powderham <edpowderham@outlook.com>
2016-11-03 11:54:43 +0000

    Fixing failing tests

--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -45,9 +45,7 @@ feature "Make booking" do
     sign_in(user)
     create_listing(listing)
     visit '/listings'
-      within('div#listing_4') do
-        click_button('Request Booking')
-      end
+    within('div#listing_9') do click_button('Request Booking') end
     expect(page).to have_current_path('/bookings/new')
     expect(page).to have_content('Booking request sent!')
     click_button('Return to listings')
